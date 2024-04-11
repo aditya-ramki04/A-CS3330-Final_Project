3b253debce7dd39bfba3dbf0395ae54121a37fcd
Matthew Robison <mattrobi04@gmail.com>
2024-04-11 18:00:44 -0500

    Created the maven project for Pacman

---
+++ PacManMain/src/main/java/edu/mu/PacManMain/Application.java
@@ -0,0 +1,8 @@
+package edu.mu.PacManMain;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ PacManMain/src/test/java/edu/mu/PacManMain/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.mu.PacManMain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
