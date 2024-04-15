de8deff4b94c5a3c3d52f9ffc4fac416da40d893
abhi pasupuleti <abhi25.pasupuleti@gmail.com>
2024-04-15 17:44:38 -0500

    Worked on the logic for a ghost

--- PacManMain/src/main/java/edu/mu/PacManMain/CyanGhost.java
+++ PacManMain/src/main/java/edu/mu/PacManMain/CyanGhost.java
@@ -1,10 +1,73 @@
 package edu.mu.PacManMain;
-
+import java.util.*;
 public class CyanGhost {
 
-	public CyanGhost() {
-		// TODO Auto-generated constructor stub
-		System.out.println("Cyan ghost test");
+	private int x, y; // position
+    private Movement direction; // current direction
+    private Random random;
+
+    public void Ghost(int startX, int startY) 
+    {
+    	x = startX;
+    	y = startY;
+	    direction = Movement.LEFT; // initial direction
+	    random = new Random();
+    }
+
+    public void move() 
+    {
+        // Implement ghost movement logic here
+        // For example, you can randomly change direction or follow a predefined path
+
+        // Here's a simple example of random movement
+        switch (random.nextInt(4)) 
+        {
+            case 0:
+                direction = Movement.UP;
+                break;
+            case 1:
+                direction = Movement.DOWN;
+                break;
+            case 2:
+                direction = Movement.LEFT;
+                break;
+            case 3:
+                direction = Movement.RIGHT;
+                break;
+            default:
+                break;
+        }
+
+        // Update position based on direction
+        switch (direction) 
+        {
+            case UP:
+                y--;
+                break;
+            case DOWN:
+                y++;
+                break;
+            case LEFT:
+                x--;
+                break;
+            case RIGHT:
+                x++;
+                break;
+            default:
+                break;
+        }
+        
+    }
+
+	    // Getter methods for position
+	    public int getX() 
+	    {
+	        return x;
+	    }
+
+	    public int getY() 
+	    {
+	        return y;
+	    }
 	}
 
-}
